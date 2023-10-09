fb24a26a8039d57e386d168d3a059babe269ec23
Tam Le <tamminhle2002@gmail.com>
2023-10-09 10:49:39 +0700

    update movie controller

--- MovieServices/Controllers/Movie/MoviesController.cs
+++ MovieServices/Controllers/Movie/MoviesController.cs
@@ -58,12 +58,13 @@ namespace MovieServices.Controllers.Movie
 
         [Authorize(Roles = "Admin")]
         [HttpPost("Create")]
-        public ActionResult<ServiceResponse<Models.Movie>> CreateMovie(AddMovieDto addMovieDto)
+        public ActionResult<ServiceResponse<MovieResponse>> CreateMovie(AddMovieDto addMovieDto)
         {
             Models.Movie movie = _mapper.Map<Models.Movie>(addMovieDto);
             movie = service.CreateMovie(movie, addMovieDto.Categories);
-            var response = new ServiceResponse<Models.Movie>();
-            response.Data = movie;
+            var movieResponse = _mapper.Map<MovieResponse>(movie);
+            var response = new ServiceResponse<MovieResponse>();
+            response.Data = movieResponse;
             response.Message = "Create Movie";
             response.Status = 200;
             return response;
@@ -71,13 +72,14 @@ namespace MovieServices.Controllers.Movie
 
         [Authorize(Roles = "Admin")]
         [HttpPut("Update")]
-        public ActionResult<ServiceResponse<Models.Movie>> UpdateMovie(UpdateMovieDto updateMovieDto)
+        public ActionResult<ServiceResponse<MovieResponse>> UpdateMovie(UpdateMovieDto updateMovieDto)
         {
             Models.Movie movie = _mapper.Map<Models.Movie>(updateMovieDto);
 
             movie = service.UpdateMovie(movie, updateMovieDto.Categories);
-            var response = new ServiceResponse<Models.Movie>();
-            response.Data = movie;
+            var movieResponse = _mapper.Map<MovieResponse>(movie);
+            var response = new ServiceResponse<MovieResponse>();
+            response.Data = movieResponse;
             response.Message = "Update Movie";
             response.Status = 200;
             return response;
@@ -85,11 +87,12 @@ namespace MovieServices.Controllers.Movie
 
         [Authorize(Roles = "Admin")]
         [HttpPut("Delete")]
-        public ActionResult<ServiceResponse<Models.Movie>> DeleteMovie(int id)
+        public ActionResult<ServiceResponse<MovieResponse>> DeleteMovie(int id)
         {
             Models.Movie movie = service.DeleteMovie(id);
-            var response = new ServiceResponse<Models.Movie>();
-            response.Data = movie;
+            var movieResponse = _mapper.Map<MovieResponse>(movie);
+            var response = new ServiceResponse<MovieResponse>();
+            response.Data = movieResponse;
             response.Message = "Delete Movie";
             response.Status = 200;
             return response;
